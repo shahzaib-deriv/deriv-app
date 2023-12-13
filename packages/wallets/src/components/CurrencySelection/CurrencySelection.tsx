26193e004883aacbd6688dda7374a075149351fa
Shaheer <shaheer@deriv.com>
2023-12-13 16:05:54 +0400

    feat: :sparkles: adds currency selection screen

--- packages/wallets/src/components/CurrencySelection/CurrencySelection.tsx
+++ packages/wallets/src/components/CurrencySelection/CurrencySelection.tsx
@@ -1,8 +1,54 @@
 import React from 'react';
+import useDevice from '../../hooks/useDevice';
+import { IconButton, WalletButton, WalletText } from '../Base';
+import useCurrencySelectionContext from './useCurrencySelectionContext';
 import './CurrencySelection.scss';
 
 const CurrencySelection = () => {
-    return <h3>Currency Selection</h3>;
+    const { isDesktop, isMobile } = useDevice();
+    const { cryptoCurrencies, currency, fiatCurrencies, setCurrency } = useCurrencySelectionContext();
+
+    return (
+        <div className='currency-selection--container'>
+            <div className='currency-selection--content'>
+                {isDesktop && (
+                    <WalletText as='div' size='md' weight='bold'>
+                        Select your preferred currency
+                    </WalletText>
+                )}
+                <div className='currency-selection--currencies'>
+                    <WalletText align='center' as='div' size='md' weight='bold'>
+                        Fiat Currencies
+                    </WalletText>
+                    <div className='currency-selection--currencies-list'>
+                        {fiatCurrencies.map(fiatCurrency => {
+                            return <IconButton icon={`IcCurrency-${fiatCurrency.code}`} key={fiatCurrency.code} />;
+                        })}
+                    </div>
+                    <WalletText align='center' size='sm'>
+                        You are limited to one fiat account. You won’t be able to change your account currency if you
+                        have already made your first deposit or created a real Deriv MT5 or Deriv X account.
+                    </WalletText>
+                </div>
+                <div className='currency-selection--border-bottom' />
+                <div className='currency-selection--currencies'>
+                    <WalletText align='center' as='div' size='md' weight='bold'>
+                        Cryptocurrencies
+                    </WalletText>
+                    <div className='currency-selection--currencies-list'>
+                        {cryptoCurrencies.map(cryptoCurrency => {
+                            return <IconButton icon={`IcCurrency-${cryptoCurrency.code}`} key={cryptoCurrency.code} />;
+                        })}
+                    </div>
+                </div>
+            </div>
+            <div className='currency-selection--footer currency-selection--border-top'>
+                <WalletButton disabled={!currency} isFullWidth={isMobile} size='lg'>
+                    Next
+                </WalletButton>
+            </div>
+        </div>
+    );
 };
 
 export default CurrencySelection;
---
+++ packages/wallets/src/components/CurrencySelection/useCurrencySelectionContext.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import { useCurrencyConfig } from '@deriv/api';
+
+const CURRENCY_TYPE = Object.freeze({
+    CRYPTO: 'crypto',
+    FIAT: 'fiat',
+});
+
+const useCurrencySelectionContext = () => {
+    const [currency, setCurrency] = useState();
+    const { data: currencyConfig } = useCurrencyConfig();
+
+    const currencies = Object.values(currencyConfig ?? {});
+    const cryptoCurrencies = Array.from(
+        currencies.filter(currencyObject => currencyObject.type === CURRENCY_TYPE.CRYPTO)
+    );
+    const fiatCurrencies = Array.from(currencies.filter(currencyObject => currencyObject.type === CURRENCY_TYPE.FIAT));
+
+    return { cryptoCurrencies, currency, fiatCurrencies, setCurrency };
+};
+
+export default useCurrencySelectionContext;
